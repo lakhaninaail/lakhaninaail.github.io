418c9e219903765dd9ccf79eb1659005d2106c50
Naail <naail.lakhani02@gmail.com>
2024-12-03 23:39:52 -0500

    updated site

--- src/components/Pages/AboutMe.js
+++ src/components/Pages/AboutMe.js
@@ -5,10 +5,7 @@ function AboutMe() {
 	return (
 		<section>
 			<h1>Hey, I'm Naail.</h1>
-			<p>
-				I'm an MIT-trained computer scientist interested in game theory, web
-				development, and AI.
-			</p>
+			<p>I'm an MIT-trained computer scientist.</p>
 			<p>
 				In the past, I've engineered data pipelines at{" "}
 				<a href="https://www.twosigma.com/" class="hyperlink">
@@ -29,12 +26,7 @@ function AboutMe() {
 				<a href="https://poker.mit.edu/" class="hyperlink">
 					MIT Poker Club
 				</a>
-				. I helped lead the first ever women's only poker tournament in
-				Cambridge, Queen's Full House, and was an instuctor for 15.S50, a poker
-				theory course taken by 100+ MIT students over IAP.
-			</p>
-			<p>
-				Now, I'm working on creating an elo-based poker matchmaking platform.
+				.
 			</p>
 		</section>
 	);
